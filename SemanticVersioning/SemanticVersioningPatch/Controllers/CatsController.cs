6179150aa6b52eb45a1351a79419b4a045e5ca99
George Bell <george@cattlebell.com>
2020-07-14 20:13:00 +0100

    Added Patch

---
+++ SemanticVersioning/SemanticVersioningPatch/Controllers/CatsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SemanticVersioningPatch.Repositories;
+
+namespace SemanticVersioningPatch.Controllers
+{
+    [ApiController]
+    [Route("cats")]
+    public class CatsController : ControllerBase
+    {
+        private readonly CatsRepository _catsRepository;
+
+        public CatsController()
+        {
+            _catsRepository = new CatsRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_catsRepository.GetCats());
+        }
+
+        [HttpGet]
+        [Route("{catName}")]
+        public IActionResult GetNamedCat(string catName)
+        {
+            return Ok(_catsRepository.GetCat(catName));
+        }
+    }
+}
---
+++ SemanticVersioning/SemanticVersioningPatch/Models/Cat.cs
@@ -0,0 +1,11 @@
+﻿namespace SemanticVersioningPatch.Models
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Age { get; set; }
+    }
+}
