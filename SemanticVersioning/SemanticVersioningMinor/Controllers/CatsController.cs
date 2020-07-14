86e4a9b4e349b4317fbe81485116a1bdcf90d98c
George Bell <george@cattlebell.com>
2020-07-14 20:36:30 +0100

    Added Minor

---
+++ SemanticVersioning/SemanticVersioningMinor/Controllers/CatsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SemanticVersioningMinor.Repositories;
+
+namespace SemanticVersioningMinor.Controllers
+{
+    [ApiController]
+    [Route("cats")]
+    public class CatsController : ControllerBase
+    {
+        private readonly CatsRepository _catsRepository;
+
+        public CatsController()
+        {
+            _catsRepository = new CatsRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_catsRepository.GetCats());
+        }
+
+        [HttpGet]
+        [Route("{catName}")]
+        public IActionResult GetNamedCat(string catName)
+        {
+            return Ok(_catsRepository.GetCat(catName));
+        }
+
+        [HttpGet]
+        [Route("NewName")]
+        public IActionResult GetNewCatName()
+        {
+            return Ok(_catsRepository.GetNewName());
+        }
+    }
+}
---
+++ SemanticVersioning/SemanticVersioningMinor/Models/Cat.cs
@@ -0,0 +1,11 @@
+﻿namespace SemanticVersioningMinor.Models
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Age { get; set; }
+    }
+}
